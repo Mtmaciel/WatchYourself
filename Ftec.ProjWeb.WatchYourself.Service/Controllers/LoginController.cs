cb5f713d6aa077aa07a055b79c0f46d647ce3c7b
Fisherman <matheuspaesmaciel@gmail.com>
2020-05-05 02:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ Ftec.ProjWeb.WatchYourself.Service/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ftec.ProjWeb.WatchYourself.Service.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+
+        public ActionResult RememberPassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ftec.ProjWeb.WatchYourself.Service/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ftec.ProjWeb.WatchYourself.Service.Models
+{
+    public class User
+    {
+
+        public Guid Id { get; set; }
+
+        public String Name { get; set; }
+
+        public byte[] Password { get; set; }
+
+        public byte[] Salt { get; set; }
+
+        public String Phone { get; set; }
+
+        public String Email { get; set; }
+
+        public DateTime LastUpdateDate { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        
+    }
+}
