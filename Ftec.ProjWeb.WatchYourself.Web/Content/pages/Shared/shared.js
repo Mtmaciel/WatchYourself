9ff19eb50f90c2658ec2e317ed566dcc83eeb328
Fisherman <matheuspaesmaciel@gmail.com>
2020-07-13 02:42:36 -0300

    ajustes de front

---
+++ Ftec.ProjWeb.WatchYourself.Web/Content/pages/Shared/shared.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('.nav-item').on('click', function () {
+        var item = $(this);
+        if (item.hasClass('active')) {
+            item.removeClass('active')
+        } else {
+            item.addClass('active')
+        }
+    })
+});
+ 
--- Ftec.ProjWeb.WatchYourself.Web/Controllers/HomeController.cs
+++ Ftec.ProjWeb.WatchYourself.Web/Controllers/HomeController.cs
@@ -13,18 +13,12 @@ namespace Ftec.ProjWeb.WatchYourself.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Ranking()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
